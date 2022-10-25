e21273db1b4938328a239b3a9de025a201649f8b
Jessica Ferreira <samojess@gmail.com>
2022-10-25 10:56:02 -0300

    finaliza requisitos 6 e 7

---
+++ src/components/Missions.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Title from './Title';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headline="Missions" />
+      </div>
+    );
+  }
+}
+
+export default Missions;
